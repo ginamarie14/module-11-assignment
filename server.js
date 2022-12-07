b953fbba6ba3c57bfa505f5d7d590a718d27cd81
Gina Marie <ginamarie214@gmail.com>
2022-12-06 23:17:50 -0600

    updated server, json should be updated, need to figure out error at index 48

--- server.js
+++ server.js
@@ -1,24 +1,32 @@
-const express = require("express");
-const fs = require("fs");
-const path = require("path");
+//dependencies & port
+const express = require('express');
+const fs = require('fs');
+const path = require('path');
 const app = express();
-const api = require("./routes/api");
-const api = require("./routes/html");
-const PORT = process.env.PORT || 3001;
+const api = require('./routes/api');
+const html = require('./routes/html');
+const PORT = process.env.PORT || 5000;
 
-app.use(express.json());
+//middleware
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static("public"));
-app.use("/api", api);
-
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "public/index.html"));
-});
-
-app.get("/notes", (req, res) => {
-  res.sendFile(path.join(__dirname, "public/notes.html"));
-});
+app.use(express.json());
+app.use(express.static(__dirname, '/public/assets'));
+app.use('/routes/api', api);
+app.use('/routes/html', html);
 
 app.listen(PORT, () => {
-  console.log(`Example app listening at http://localhost:${PORT}`);
+    console.log(`Open me here: http://localhost:${PORT}`);
+  });
+
+//routes
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '/public/index.html'));
+});
+
+app.get('/notes', (req, res) => {
+  res.sendFile(path.join(__dirname, '/public/notes.html'));
+});
+
+app.get('/api/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, '/db/db.json'))
 });
